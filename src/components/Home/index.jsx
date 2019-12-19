c48986dc61a09c4e71067875e0cb920c8f8ecf27
itsme <ss.abhilash3@gmail.com>
2019-12-19 17:14:17 +0530

    Add progress bar and show population percentage

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,21 +1,82 @@
 import React, { useState } from "react";
-import { Row, Col, Input } from "antd";
+import { Row, Col, Input, Card, Progress } from "antd";
 import { getPlanets } from "../../services";
 
 const { Search } = Input;
 
 const Home = () => {
   const [planets, setplanets] = useState([]);
-//   const [totalPopulation, setTotalPopulation] = useState(0);
+  const [totalPopulation, setTotalPopulation] = useState(0);
+
   const searchPlanet = async searchTerm => {
     const res = await getPlanets(searchTerm);
     if (res) {
       setplanets(res);
+      const totalPop = res.length
+        ? res.reduce(
+            (acc, cv) =>
+              Number(acc) +
+              (cv.population && cv.population !== "unknown"
+                ? Number(cv.population)
+                : 0),
+            0
+          )
+        : 0;
+      setTotalPopulation(totalPop);
     }
   };
 
   const renderPlanets = planets => {
-    return planets.map(planet => <Col>{planet.name}</Col>);
+    const knownPlanets = planets.filter(
+      planet => planet.name !== "unknown" && planet.population !== "unknown"
+    );
+
+    knownPlanets.sort((a, b) => Number(b.population) - Number(a.population));
+
+    return knownPlanets.map((planet, index) => (
+      <Col xs={24} md={8} key={index}>
+        <Card
+          size="small"
+          title={planet.name}
+          //   extra={<a href="#">More</a>}
+          //   style={{ width: 300 }}
+        >
+          Landscape: <b>{planet.terrain}</b>
+          <br />
+          Gravity: {"  "}
+          <b>
+            {planet.gravity && planet.gravity !== "N/A"
+              ? planet.gravity
+              : "1 standard"}
+          </b>
+          <br />
+          Population: <b>{planet.population}</b>
+          <br />
+          {console.info(
+            "population",
+            planet.name,
+            planet.population,
+            totalPopulation,
+            planet.population / totalPopulation
+          )}
+          <strong>
+            {((planet.population * 100) / totalPopulation).toFixed(3)}%
+          </strong>{" "}
+          of total population from current list.
+          <Progress
+            strokeColor={{
+              from: "#108ee9",
+              to: "#87d068"
+            }}
+            // type="circle"
+            percent={Number(
+              ((planet.population * 100) / totalPopulation).toFixed(3)
+            )}
+            // format={percent => ``}
+          />
+        </Card>
+      </Col>
+    ));
   };
 
   return (
@@ -31,7 +92,9 @@ const Home = () => {
           />
         </Col>
       </Row>
-      <Row>{renderPlanets(planets)}</Row>
+      <Row gutter={[32, 32]} style={{ marginTop: "10px" }}>
+        {renderPlanets(planets)}
+      </Row>
     </>
   );
 };
